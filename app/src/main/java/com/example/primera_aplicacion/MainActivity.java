5d8fea7cf1723c22616b14f7cfe99a0c702064d9
DanielGonzalez101 <159564649+DanielGonzalez101@users.noreply.github.com>
2024-03-08 17:00:18 -0500

    Ya la calculadora sirve

--- app/src/main/java/com/example/primera_aplicacion/MainActivity.java
+++ app/src/main/java/com/example/primera_aplicacion/MainActivity.java
@@ -9,29 +9,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //public void enviar(View view) {
-     //   Toast.makeText(this, "Prueba", Toast.LENGTH_SHORT).show();
-    //}
-
+    //Llamamos los dos valores
     EditText edtVal1, edtVal2;
+    //Este textView es para ver el resultado directamente no se ve si no hay un resultado
+    TextView txtResult;
+    //LLamammos boton calcular
     Button btnCacular;
 
-    //public float calcular(EditText edtVal1, EditText edtVal2){
-     //   float resultado =
+    public void calcularSuma(View view) {
+        //Extraemos todos los valores de el xml
+        EditText edtVal1 = findViewById(R.id.edtVal1);
+        EditText edtVal2 = findViewById(R.id.edtVal2);
+        TextView txtResult = findViewById(R.id.txtResult);
 
-    //}
+        //Convertimos los valores a double para poder usarlos tipo numerico (sirve con otros tipos)
+        double value1 = Double.parseDouble(edtVal1.getText().toString());
+        double value2 = Double.parseDouble(edtVal2.getText().toString());
+
+        //Los sumamos
+        double sum = value1 + value2;
+
+        //LOs mostramos en la pantalla
+        txtResult.setText(String.valueOf(sum));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        edtVal1 = findViewById(R.id.edtVal1);
-        edtVal2 = findViewById(R.id.edtVal2);
-
-
     }
 }
